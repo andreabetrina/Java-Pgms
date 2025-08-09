ede1a4481a81f368db2717669f751be81b78392a
Andrea Betrina P <andreabetrina06@gmail.com>
2025-08-08 23:33:39 -0700

    Create MarkAvg.java

---
+++ MarkAvg.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Stu {
+    public static void main(String[] args) {
+        String name;
+        int rno;
+        int phy, chem, mat, tot;
+        float avg;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the name:");
+        name = sc.next();
+
+        System.out.println("Enter the roll number:");
+        rno = sc.nextInt();
+
+        System.out.println("Enter Physics mark:");
+        phy = sc.nextInt();
+
+        System.out.println("Enter Chemistry mark:");
+        chem = sc.nextInt();
+
+        System.out.println("Enter Maths mark:");
+        mat = sc.nextInt();
+
+        tot = phy + chem + mat;
+        System.out.println("Total = " + tot);
+
+        avg = tot / 3.0f; // Ensures float division
+        System.out.println("Average = " + avg);
+    }
+}
